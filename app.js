96232a47155bb5f8ec3f61ef9ef9c5b174d6e9e6
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-10-15 15:28:24 +0200

    settata rotta bacheca

--- app.js
+++ app.js
@@ -3,12 +3,54 @@ const express = require("express");
 // creiamo un istanza di express
 const app = express();
 // numero della porta da utilizzare
-const port = 3000;
+const port = 3001;
 
 app.use(express.static('public'));
 
 // rotta di default
-app.get("/",(rep, res) =>{
+app.get("/",(req, res) =>{
     res.send(`<h1> Server del mio blog </h1>`)
 })
 
+// rotta della bacheca
+app.get("/bacheca",(req, res) =>{
+    const posts = [
+        {
+            titolo: "ciambellone",
+            contenuto: "ciambella grossa",
+            image: "images/ciambellone.jpeg",
+            tags: ["ciambellone","cibo","ciambella"]
+        },
+        {
+            titolo: "cracker",
+            contenuto: "cracker alla barbabietola",
+            image: "images/cracker_barbabietola.jpeg",
+            tags: ["cracker","cibo","barbabietola"]
+        },
+        {
+            titolo: "pane fritto",
+            contenuto: "pane fritto dolce",
+            image: "images/pane_fritto_dolce.jpeg",
+            tags: ["pane","cibo","fritto"]
+        },
+        {
+            titolo: "pasta barbabietola",
+            contenuto: "pasta alla barbabietola",
+            image: "images/pasta_barbabietola.jpeg",
+            tags: ["pasta","cibo","barbabietola"]
+        },
+        {
+            titolo: "torta paesana",
+            contenuto: "torta di paese",
+            image: "images/torta_paesana.jpeg",
+            tags: ["torta","cibo","paesana"]
+        }
+    ]
+
+    res.json(posts);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
